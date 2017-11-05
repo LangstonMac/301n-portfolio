d52eae62184ddb028f4ca6470a35371290d19ddc
Langston McClarron <langstunned@gmail.com>
2017-11-05 09:18:00 -0800

    added jquery for portfolio 2

---
+++ scripts/data.js
@@ -0,0 +1,21 @@
+//array of data for the projects array
+var projectData = [
+  {
+    title: 'Voting Project',
+    finalizedOn: 'September 3rd, 2017',
+    body: '<p> The Voting Project was a site built in order to replicate somewhere where you could vote on images you liked the most and accumilate that data. Upon recording 15 selections of images the site brings up a graph displaying your choices. The great thing about this project was that once you vote again (even if you reload the page) it records the data you have input keeping it in local storage and continues the votes accordingly! Much wow!</p>',
+    projectUrl:'https://github.com/LangstonMac/Voting-Project'
+  },
+  {
+    title:'Salmon Cookie Project',
+    finalizedOn:'August 28th, 2017',
+    body:'<p> The Salmon Cookie Project was built to simulate a site where you can view the different areas of Portland that have a Salmon Cookie stand and how much they were making at each location. The functionality of this site that brought it to life was being able to add information to the site via a form.</p>',
+    projectUrl:'https://github.com/LangstonMac/salmoncookieproject'
+  },
+  {
+    title:'Guessing Game',
+    finalizedOn:'August 14th, 2017',
+    body:'<p> The Guessing Game was my first real project. It involved a call and response involving the site and the user where the user would be asked questions about me and have to give answers. There was information about me at the end of the game and a showing of how many answers were right or wrong.</p>',
+    projectUrl:'https://github.com/LangstonMac/guessing-game'
+  },
+];
